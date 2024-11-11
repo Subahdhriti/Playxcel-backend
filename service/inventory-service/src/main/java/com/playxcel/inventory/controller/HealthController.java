88ab7569bfb53b9604bee28ef812215de3195860
Subhadhriti Maikap <subhadhritimaikap@gmail.com>
2024-11-11 10:07:32 +0530

    health route added

--- service/inventory-service/src/main/java/com/playxcel/inventory/controller/HealthController.java
+++ service/inventory-service/src/main/java/com/playxcel/inventory/controller/HealthController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("v1/items")
 public class HealthController {
 
     @GetMapping("/health")
